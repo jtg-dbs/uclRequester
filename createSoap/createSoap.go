9374cfa653874ac2e1a68bf8658e4b13278ec3a1
jannthorbengraef <jann-thorben.graef@deutschebahn.com>
2021-04-30 17:32:37 +0200

    add body for request, rms http headers are missing

--- createSoap/createSoap.go
+++ createSoap/createSoap.go
@@ -1,6 +1,8 @@
 package createSoap
 
 import (
+	"bytes"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
@@ -21,14 +23,22 @@ func UclSoapRequest(mrl string, clc string, gic string) {
 		"Content-Type":    "text/xml; charset=utf-8",
 		"Accept-Encoding": "gzip, deflate",
 		"SOAPAction":      `"http://microsoft.com/DRM/LicensingService/AcquireLicense"`,
+		"Accept":          "*/*",
+		"Connection":      "Keep-Alive",
 	}
 	soapClient := &http.Client{
 		Transport: &spnego.Transport{},
 	}
-
-	url := getUrl(gic, uclSoapRequestLog) + "/licensing/License.amx"
+	xmlBody := newEnvelope()
+	body, err := xml.Marshal(xmlBody)
+	if err != nil {
+		uclSoapRequestLog.Print(err)
+	}
+	bodyFilled := insertContent(mrl, clc, gic, string(body))
+	uclSoapRequestLog.Print(bodyFilled)
+	url := getUrl(gic, uclSoapRequestLog) + "/licensing/License.asmx"
 	uclSoapRequestLog.Print(url)
-	soapRequest, err := http.NewRequest("POST", url, nil) // XML Document has to be in body
+	soapRequest, err := http.NewRequest("POST", url, bytes.NewReader([]byte(bodyFilled))) // XML Document has to be in body
 	for key, value := range headers {
 		soapRequest.Header.Set(key, value)
 	}
@@ -38,6 +48,18 @@ func UclSoapRequest(mrl string, clc string, gic string) {
 	resp, err := soapClient.Do(soapRequest)
 	uclSoapRequestLog.Print(err)
 	uclSoapRequestLog.Print(resp)
+}
+
+func insertContent(mrl string, clc string, gic string, bodyEmpty string) (bodyFilled string) {
+	IssuanceLicense := `<IssuanceLicense soapenc:arrayType="Certificate[2]">`
+	LicenseeCerts := `<LicenseeCerts soapenc:arrayType="Certificate[1]">`
+	xmlHead := `<?xml version="1.0" encoding="utf-8"?>`
+	indexArray1 := strings.Index(bodyEmpty, IssuanceLicense) + len(IssuanceLicense)
+	body := bodyEmpty[:indexArray1] + "<Certificate>" + mrl + "</Certificate>" + "<Certificate>" + clc + "</Certificate>" + bodyEmpty[indexArray1:]
+	indexArray2 := strings.Index(body, LicenseeCerts) + len(LicenseeCerts)
+	bodyFilled = body[:indexArray2] + "<Certificate>" + gic + "</Certificate>" + body[indexArray2:]
+	bodyFilled = xmlHead + bodyFilled
+	return bodyFilled
 }
 
 //extracts the ad rms server url from the gic
@@ -56,4 +78,62 @@ func getUrl(gic string, log *log.Logger) (url string) {
 func getWinVersion(log *log.Logger) (version string) {
 	versionWin := w32.RtlGetVersion()
 	return fmt.Sprint(versionWin.MajorVersion) + "." + fmt.Sprint(versionWin.MinorVersion) + "." + fmt.Sprint(versionWin.BuildNumber)
+}
+
+type Envelope struct {
+	XMLName xml.Name `xml:"soap:Envelope"`
+	Text    string   `xml:",chardata"`
+	Soap    string   `xml:"xmlns:soap,attr"`
+	Xsi     string   `xml:"xmlns:xsi,attr"`
+	Xsd     string   `xml:"xmlns:xsd,attr"`
+	Soapenc string   `xml:"xmlns:soapenc,attr"`
+	Header  struct {
+		Text        string `xml:",chardata"`
+		VersionData struct {
+			Text           string `xml:",chardata"`
+			Xmlns          string `xml:"xmlns,attr"`
+			MinimumVersion string `xml:"MinimumVersion"`
+			MaximumVersion string `xml:"MaximumVersion"`
+		} `xml:"VersionData"`
+	} `xml:"soap:Header"`
+	Body struct {
+		Text           string `xml:",chardata"`
+		AcquireLicense struct {
+			Text          string `xml:",chardata"`
+			Xmlns         string `xml:"xmlns,attr"`
+			RequestParams struct {
+				Text                 string `xml:",chardata"`
+				Xmlns                string `xml:"xmlns,attr"`
+				AcquireLicenseParams struct {
+					Text            string `xml:",chardata"`
+					IssuanceLicense struct {
+						Text        string   `xml:",chardata"`
+						ArrayType   string   `xml:"soapenc:arrayType,attr"`
+						Certificate []string `xml:"Certificate"`
+					} `xml:"IssuanceLicense"`
+					LicenseeCerts struct {
+						Text        string   `xml:",chardata"`
+						ArrayType   string   `xml:"soapenc:arrayType,attr"`
+						Certificate []string `xml:"Certificate"`
+					} `xml:"LicenseeCerts"`
+				} `xml:"AcquireLicenseParams"`
+			} `xml:"RequestParams"`
+		} `xml:"AcquireLicense"`
+	} `xml:"soap:Body"`
+}
+
+func newEnvelope() Envelope {
+	envelope := Envelope{}
+	envelope.Soap = "http://schemas.xmlsoap.org/soap/envelope/"
+	envelope.Xsi = "http://www.w3.org/2001/XMLSchema-instance"
+	envelope.Xsd = "http://www.w3.org/2001/XMLSchema"
+	envelope.Soapenc = "http://schemas.xmlsoap.org/soap/encoding/"
+	envelope.Header.VersionData.Xmlns = "http://microsoft.com/DRM/LicensingService"
+	envelope.Header.VersionData.MaximumVersion = "1.0.0.0"
+	envelope.Header.VersionData.MinimumVersion = "1.0.0.0"
+	envelope.Body.AcquireLicense.Xmlns = "http://microsoft.com/DRM/LicensingService"
+	envelope.Body.AcquireLicense.RequestParams.Xmlns = "http://microsoft.com/DRM/LicensingService"
+	envelope.Body.AcquireLicense.RequestParams.AcquireLicenseParams.IssuanceLicense.ArrayType = "Certificate[2]"
+	envelope.Body.AcquireLicense.RequestParams.AcquireLicenseParams.LicenseeCerts.ArrayType = "Certificate[1]"
+	return envelope
 }
